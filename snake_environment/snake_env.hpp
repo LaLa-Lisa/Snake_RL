6ff8789fad375c74bf245e9903fd057f6cf56c2c
LaLa-Lisa <lyohich999@ya.ru>
2022-10-17 21:30:21 +0300

    env fixes
    emm...

--- snake_environment/snake_env.hpp
+++ snake_environment/snake_env.hpp
@@ -14,14 +14,14 @@ public:
 	void reset() {
 		gameOver = false;
 		tailN = 0;
-		sorce = 0;
+		score = 0;
 		TailX.resize(wight * heght, -1);
 		TailY.resize(wight * heght, -1);
 		x = wight / 2;
 		y = heght / 2;
 		while (true) {
-			FrutX = rand() % (heght - 2) + 1;
-			FrutY = rand() % (wight);
+			FrutX = rand() % heght;
+			FrutY = rand() % wight;
 			if (FrutX == x && FrutY == y) continue;
 			break;
 		}
@@ -29,14 +29,14 @@ public:
 	}
 	void console_render() {
 		setcur(0, 0);
-		for (int i = 0; i < heght; i++) {
+		for (int i = 0; i < heght + 2; i++) {
 			std::cout << "#";
 		}
 		std::cout << std::endl;
-		for (int i = 0; i < wight; i++) {
-			for (int j = 0; j < heght - 1; j++) {
-				if (j == 0) std::cout << "#";
-				else if (j == x && i == y) std::cout << "O";
+		for (int i = 0; i < wight; ++i) {
+			std::cout << "#";
+			for (int j = 0; j < heght; ++j) {
+				if (j == x && i == y) std::cout << "O";
 				else if (j == FrutX && i == FrutY) std::cout << "F";
 				else {
 					bool by = true;
@@ -48,70 +48,67 @@ public:
 					}
 					if (by) std::cout << " ";
 				}
-
-				if (j == heght - 2) std::cout << "#";
-
 			}
-			std::cout << std::endl;
+			std::cout << "#" << std::endl;
 		}
-		for (int i = 0; i < heght; i++) {
+		for (int i = 0; i < heght + 2; i++) {
 			std::cout << "#";
 		}
 		std::cout << std::endl;
-		std::cout << "Sorce:" << sorce << std::endl;
-		std::cout << "FrutCoords:" << FrutX << " " << FrutY << "\n";
+		std::cout << "Sorce:" << score << std::endl;
+		std::cout << "FrutCoords:" << FrutX << " " << FrutY << std::endl;
 	}
 	void step(const int _input_direction) {
 		Input(_input_direction);
 		Logic();
 	}
 	std::vector<double> observe() {
-		double sensor1 = (x - 1);
-		double sensor2 = (y);
-		double sensor3 = (heght - 2 - x);
-		double sensor4 = (wight - y - 1);
-		double diag1 = 0;
-		double diag2 = 0;
-		double diag3 = 0;
-		double diag4 = 0;
-		for (int i = 0; i < min(sensor1, sensor2); i++)
-			for (int k = 0; k < tailN; k++)
+		double sensor1 = x;
+		double sensor2 = y;
+		double sensor3 = heght - x - 1;
+		double sensor4 = wight - y - 1;
+		double diag1 = -1;
+		double diag2 = -1;
+		double diag3 = -1;
+		double diag4 = -1;
+		for (int i = 0; i < min(sensor1, sensor2); ++i)
+			for (int k = 0; k < tailN; ++k)
 				if (x - i == TailX[k] && y - i == TailY[k]) {
-					diag1 = 1;
-					i = 10000;
+					diag1 = i;
+					i = min(sensor1, sensor2);
 					break;
 				}
-		for (int i = 0; i < min(sensor2, sensor3); i++)
-			for (int k = 0; k < tailN; k++)
+		for (int i = 0; i < min(sensor2, sensor3); ++i)
+			for (int k = 0; k < tailN; ++k)
 				if (x + i == TailX[k] && y - i == TailY[k]) {
-					diag2 = 1;
-					i = 10000;
+					diag2 = i;
+					i = min(sensor2, sensor3);
 					break;
 				}
-		for (int i = 0; i < min(sensor3, sensor4); i++)
-			for (int k = 0; k < tailN; k++)
+		for (int i = 0; i < min(sensor3, sensor4); ++i)
+			for (int k = 0; k < tailN; ++k)
 				if (x + i == TailX[k] && y + i == TailY[k]) {
-					diag3 = 1;
-					i = 10000;
+					diag3 = i;
+					i = min(sensor3, sensor4);
 					break;
 				}
-		for (int i = 0; i < min(sensor4, sensor1); i++)
-			for (int k = 0; k < tailN; k++)
+		for (int i = 0; i < min(sensor4, sensor1); ++i)
+			for (int k = 0; k < tailN; ++k)
 				if (x - i == TailX[k] && y + i == TailY[k]) {
-					diag4 = 1;
-					i = 10000;
+					diag4 = i;
+					i = min(sensor4, sensor1);
 					break;
 				}
 		double sensor5 = -1;
 		double sensor6 = -1;
 		double sensor7 = -1;
 		double sensor8 = -1;
-		if (x == FrutX) (y > FrutY) ? sensor6 = y - FrutY + 1 : sensor8 = FrutY - y + 1;
-		if (y == FrutY) (x > FrutX) ? sensor5 = x - FrutX + 1 : sensor7 = FrutX - x + 1;
+		if (x == FrutX) (y > FrutY) ? sensor6 = y - FrutY - 1 : sensor8 = FrutY - y - 1;
+		if (y == FrutY) (x > FrutX) ? sensor5 = x - FrutX - 1 : sensor7 = FrutX - x - 1;
 
 		for (int i = 0; i < tailN; ++i) {
-			if (x == TailX[i]) { (y > TailY[i]) ? sensor2 = min(sensor2, (y - TailY[i]) - 1) : sensor4 = min(sensor4, (TailY[i] - y) - 1); }
-			if (y == TailY[i]) (x > TailX[i]) ? sensor1 = min(sensor1, (x - TailX[i]) - 1) : sensor3 = min(sensor3, (TailX[i] - x) - 1);
+			if (x == TailX[i]) (y > TailY[i]) ? sensor2 = min(sensor2, y - TailY[i] - 1) : sensor4 = min(sensor4, TailY[i] - y - 1); 
+			if (y == TailY[i]) (x > TailX[i]) ? sensor1 = min(sensor1, x - TailX[i] - 1) : sensor3 = min(sensor3, TailX[i] - x - 1);
 		}
 		std::vector<double> ans;
 		switch (dir)
@@ -139,11 +136,14 @@ public:
 	int snake_len() {
 		return tailN;
 	}
+	double reward() {
+		return score;
+	}
 
 private:
 	const int wight, heght;
 	int x, y, FrutX, FrutY;
-	int sorce = 0;
+	int score = 0;
 	int tailN = 0;
 	std::vector<int> TailX, TailY;
 	bool gameOver;
@@ -179,21 +179,21 @@ private:
 	}
 	void Logic() {
 		if (x == FrutX && y == FrutY) {
-			sorce += 1;
+			++score;
 			while (true) {
-				FrutX = rand() % (heght - 2) + 1;
-				FrutY = rand() % (wight);
+				FrutX = rand() % heght;
+				FrutY = rand() % wight;
 				if (FrutX == x && FrutY == y) continue;
 				for (int k = 0; k < tailN + 1; k++)
 					if (FrutX == TailX[k] && FrutY == TailY[k])
 						continue;
 				break;
 			}
-			tailN++;
+			++tailN;
 		}
 		int headX = x;
 		int headY = y;
-		for (int i = tailN - 1; i > 0; i--) {
+		for (int i = tailN - 1; i > 0; --i) {
 			TailX[i] = TailX[i - 1];
 			TailY[i] = TailY[i - 1];
 		}
@@ -215,8 +215,8 @@ private:
 			break;
 		}
 		if (x == heght - 1) gameOver = true;
-		if (y == wight) gameOver = true;
-		if (x <= 0) gameOver = true;
+		if (y == wight - 1) gameOver = true;
+		if (x < 0) gameOver = true;
 		if (y < 0) gameOver = true;
 		for (int k = 1; k < tailN; k++) {
 			if (x == TailX[k] && y == TailY[k]) gameOver = true;
@@ -229,3 +229,4 @@ private:
 		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 	}
 };
+
