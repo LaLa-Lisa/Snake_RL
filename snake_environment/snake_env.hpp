9ca4a4279caa5c910af01798d2e3a3d00ab947e7
LaLa-Lisa <lyohich999@ya.ru>
2022-10-29 00:53:33 +0300

    backstep and anticircle

--- snake_environment/snake_env.hpp
+++ snake_environment/snake_env.hpp
@@ -7,12 +7,14 @@
 
 class snake_env {
 public:
-	snake_env(int _wight, int _heght): wight(_wight), heght(_heght)
+	snake_env(int _wight, int _heght): wight(_wight), heght(_heght), max_availible_steps(_wight * _heght * 2)
 	{
 		reset();
 	}
 	void reset() {
 		gameOver = false;
+		step_counter = 0;
+		steps_without_frut = 0;
 		tailN = 0;
 		score = 0;
 		TailX.resize(wight * heght, -1);
@@ -57,10 +59,72 @@ public:
 		std::cout << std::endl;
 		std::cout << "Sorce:" << score << std::endl;
 		std::cout << "FrutCoords:" << FrutX << " " << FrutY << std::endl;
+		std::cout << "Direction: " << direction() << "       " << std::endl;
 	}
 	void step(const int _input_direction) {
+		++step_counter;
+		tail_end_step.push_back(std::make_pair(TailX[tailN - 1], TailY[tailN - 1]));
+		score_step.push_back(score);
+		direction_step.push_back(dir);
+		frut_step.push_back(std::make_pair(FrutX, FrutY));
 		Input(_input_direction);
 		Logic();
+	}
+	void backstep(const int _prev_direction) {
+		Input(_prev_direction);
+		if (!step_counter) {
+			throw std::invalid_argument("no steps before");
+		}
+		/*if (dir != direction_step.end()[-1]) {
+			throw std::invalid_argument("wrong last action");
+		}*/
+
+		//hvostic
+		FrutX = frut_step.end()[-1].first;
+		FrutY = frut_step.end()[-1].second;
+
+		//fruktic
+		for (int i = 0; i < tailN; ++i) {
+			TailX[i] = TailX[i + 1];
+			TailY[i] = TailY[i + 1];
+		}
+
+		if (x == FrutX && y == FrutY) {
+			--tailN;
+			TailX.pop_back();
+			TailY.pop_back();
+		}
+		else {
+			TailX[tailN - 1] = tail_end_step.end()[-1].first;
+			TailY[tailN - 1] = tail_end_step.end()[-1].second;
+		}
+		
+
+		switch (dir)
+		{
+		case UP:
+			y++;
+			break;
+		case RIGHT:
+			x--;
+			break;
+		case LEFT:
+			x++;
+			break;
+		case DOWN:
+			y--;
+			break;
+		}
+		dir = direction_step.end()[-1];
+
+		score = score_step.end()[-1];
+
+		--step_counter;
+		tail_end_step.pop_back();
+		score_step.pop_back();
+		direction_step.pop_back();
+		frut_step.pop_back();
+
 	}
 	std::vector<double> observe() {
 		double sensor1 = x;
@@ -106,27 +170,34 @@ public:
 		if (x == FrutX) (y > FrutY) ? sensor6 = y - FrutY - 1 : sensor8 = FrutY - y - 1;
 		if (y == FrutY) (x > FrutX) ? sensor5 = x - FrutX - 1 : sensor7 = FrutX - x - 1;
 
+		double sensor9 = -1;
+		double sensor10 = -1;
+		double sensor11 = -1;
+		double sensor12 = -1;
 		for (int i = 0; i < tailN; ++i) {
-			if (x == TailX[i]) (y > TailY[i]) ? sensor2 = min(sensor2, y - TailY[i] - 1) : sensor4 = min(sensor4, TailY[i] - y - 1); 
-			if (y == TailY[i]) (x > TailX[i]) ? sensor1 = min(sensor1, x - TailX[i] - 1) : sensor3 = min(sensor3, TailX[i] - x - 1);
+			if (x == TailX[i]) (y > TailY[i]) ? sensor10 =  min(max(sensor10, sensor2), y - TailY[i] - 1) : sensor12 = min(max(sensor12, sensor4), TailY[i] - y - 1);
+			if (y == TailY[i]) (x > TailX[i]) ? sensor9 = min(max(sensor9, sensor1), x - TailX[i] - 1) : sensor11 = min(max(sensor11, sensor3), TailX[i] - x - 1);
 		}
 		std::vector<double> ans;
 		switch (dir)
 		{
 		case UP:
-			ans = { sensor1, sensor2, sensor3, sensor5, sensor6, sensor7, diag4, diag1, diag2, diag3 };
+			ans = { sensor1, sensor2, sensor3, sensor5, sensor6, sensor7, sensor9, sensor10, sensor11, diag4, diag1, diag2, diag3 };
 			break;
 		case RIGHT:
-			ans = { sensor2, sensor3, sensor4, sensor6, sensor7, sensor8, diag1, diag2, diag3, diag4 };
+			ans = { sensor2, sensor3, sensor4, sensor6, sensor7, sensor8, sensor10, sensor11, sensor12, diag1, diag2, diag3, diag4 };
 			break;
 		case LEFT:
-			ans = { sensor4, sensor1, sensor2, sensor8, sensor5, sensor6, diag2, diag3, diag4, diag1 };
+			ans = { sensor4, sensor1, sensor2, sensor8, sensor5, sensor6, sensor12, sensor9, sensor10, diag2, diag3, diag4, diag1 };
 			break;
 		case DOWN:
-			ans = { sensor3, sensor4, sensor1, sensor7, sensor8, sensor5, diag3, diag4, diag1, diag2 };
+			ans = { sensor3, sensor4, sensor1, sensor7, sensor8, sensor5, sensor11, sensor12, sensor9, diag3, diag4, diag1, diag2 };
 			break;
 		}
-		ans.push_back((double)tailN);
+		//ans.push_back((double)tailN);
+		for (auto& i : direction_output()) {
+			ans.push_back((double)i);
+		}
 		return ans;
 	}
 	bool is_done() {
@@ -139,6 +210,43 @@ public:
 	double reward() {
 		return score;
 	}
+	std::string direction() {
+		switch (dir)
+		{
+		case UP:
+			return "UP";
+			break;
+		case RIGHT:
+			return "RIGHT";
+			break;
+		case LEFT:
+			return "LEFT";
+			break;
+		case DOWN:
+			return "DOWN";
+			break;
+		}
+		return "STOP";
+	}
+
+	std::vector<int> direction_output() {
+		switch (dir)
+		{
+		case UP:
+			return { 1, 0, 0, 0 };
+			break;
+		case RIGHT:
+			return { 0, 1, 0, 0 };
+			break;
+		case LEFT:
+			return { 0, 0, 1, 0 };
+			break;
+		case DOWN:
+			return { 0, 0, 0, 1 };
+			break;
+		}
+		return { 0, 0, 0, 0 };
+	}
 
 private:
 	const int wight, heght;
@@ -149,6 +257,18 @@ private:
 	bool gameOver;
 	enum mDirection { UP, RIGHT, LEFT, DOWN };
 	mDirection dir;
+
+	//backstep
+	int step_counter = 0;
+	std::vector<std::pair<int, int>> tail_end_step;
+	std::vector<std::pair<int, int>> frut_step;
+	std::vector<int> score_step;
+	std::vector<mDirection> direction_step;
+
+	//anti circle
+	bool is_circle_restriced = true;
+	const int max_availible_steps;
+	int steps_without_frut = 0;
 
 	void Input(const int way) {
 		switch (dir)
@@ -190,6 +310,13 @@ private:
 				break;
 			}
 			++tailN;
+			steps_without_frut = 0;
+		}
+		else {
+			++steps_without_frut;
+			if (is_circle_restriced && max_availible_steps < steps_without_frut) {
+				gameOver = true;
+			}
 		}
 		int headX = x;
 		int headY = y;
